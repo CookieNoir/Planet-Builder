bd3f7ea5a714c32b0be1a4d44619b218d6d2dbf8
Alexsandr Syrykh <zsyryhz@mail.ru>
2020-06-12 14:17:12 +0300

    Basic script for game

---
+++ Syrykh/PolarSystem.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PolarSystem
+{
+    PolarSystem() { }
+
+    Vector3 position(float angle, float radius, Vector3 center)
+    {
+        Vector3 newPosition = new Vector3();
+
+        newPosition.x = (center.x + radius) * Mathf.Cos(angle);
+        newPosition.y = (center.y + radius) * Mathf.Sin(angle);
+        newPosition.z = 0;
+
+        return newPosition;
+    }
+}
